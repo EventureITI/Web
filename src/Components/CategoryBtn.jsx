4d6f541ada7f0f3b2369fde0ef364f9eb2d9fed9
Abdallah <abdallahmamdouh.eng@gmail.com>
2024-09-03 00:04:01 +0300

    style: style active category buttons

--- src/Components/CategoryBtn.jsx
+++ src/Components/CategoryBtn.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-export default function CategoryBtn({category,bg,hoverBg}) {
+export default function CategoryBtn({ category , path , classes }) {
+  
   return (
-    <button type="button" style={{height:"30px",minWidth:"80px"}} className={`flex justify-center items-center text-white ${bg} hover:${hoverBg} focus:outline-none font-medium rounded-full text-sm px-8 py-2.5 text-center mb-2 `}>{category}</button>
-  )
+    <NavLink
+      to={path}
+      style={{ height: "30px", minWidth: "80px" }}
+      className={({ isActive }) =>
+        `flex justify-center items-center bg-zinc-700 ${classes} hover:bg-main-hover text-white focus:outline-none font-medium rounded-full text-sm px-8 py-2.5 text-center mb-2 ${
+          isActive ? " focus:bg-main-color" : "bg-zinc-700"
+        }`
+      }
+    >
+      {category}
+    </NavLink>
+  );
 }
