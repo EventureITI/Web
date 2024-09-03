b696f036301e8b68799de0997726dc428c4977de
Aya Abdelmaaboud <aabdelmaaboud94@gmail.com>
2024-09-03 09:11:20 +0300

    refacot: formatting text

--- src/Components/CategoryBtn.jsx
+++ src/Components/CategoryBtn.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default function CategoryBtn({category,bg,hoverBg}) {
+export default function CategoryBtn({ category, bg, hoverBg }) {
   return (
-    <button type="button" style={{height:"30px",minWidth:"80px"}} className={`flex justify-center items-center text-white ${bg} hover:${hoverBg} focus:outline-none font-medium rounded-full text-sm px-8 py-2.5 text-center mb-2 `}>{category}</button>
-  )
+    <button
+      type="button"
+      style={{ height: "30px", minWidth: "80px" }}
+      className={`flex justify-center items-center text-white ${bg} hover:${hoverBg} focus:outline-none font-medium rounded-full text-sm px-8 py-2.5 text-center mb-2 `}
+    >
+      {category}
+    </button>
+  );
 }
