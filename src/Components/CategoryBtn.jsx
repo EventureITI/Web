c503fb15b5ea72c6a47f0b10891434ea40a93ccb
Aya Abdelmaaboud <aabdelmaaboud94@gmail.com>
2024-09-03 21:53:17 +0300

    fix: add style to category button

--- src/Components/CategoryBtn.jsx
+++ src/Components/CategoryBtn.jsx
@@ -6,7 +6,7 @@ export default function CategoryBtn({ category, path, classes }) {
       to={path}
       style={{ height: "30px", minWidth: "80px" }}
       className={({ isActive }) =>
-        `flex justify-center items-center bg-zinc-700 ${classes} hover:bg-main-hover text-white focus:outline-none font-medium rounded-full text-sm px-8 py-2.5 text-center mb-2 ${
+        `text-uppercase flex justify-center items-center bg-zinc-700 ${classes} hover:bg-main-hover text-white focus:outline-none font-medium rounded-full text-sm px-8 py-2.5 text-center mb-2 ${
           isActive ? " focus:bg-main-color" : "bg-zinc-700"
         }`
       }
