c3b8899bdcbacb313aad934fcf71e9336214cf70
Amr Assem <amr.assem@onmail.com>
2024-09-05 17:34:24 +0300

    refactor: remove unused imports and redundant code from EventsPage

--- src/Pages/EventsPage.jsx
+++ src/Pages/EventsPage.jsx
@@ -4,37 +4,18 @@ import EventCard from "../Components/Home/EventCard";
 import Pagination from "../Components/Pagination";
 import BackTop from "../Components/BackTop";
 import { appContext } from "../context/AppContext";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import generateArrayFromNumber from "../utils/generateArrayFromNumber";
 
 export default function EventsPage() {
   const [currentPage, setCurrentPage] = useState(1);
 
-  const navigate = useNavigate();
   const { events, categories } = useContext(appContext);
   const { category } = useParams();
   console.log(category);
-  const [searchEventsKey, setSearchEventsKey] = useState("");
 
   const [filteredSearch, setFilteredSearch] = useState(events);
   const [searchWord, SetSearchWord] = useState("");
-
-  const handleSearchEventsKeyChanges = (key) => {
-    setSearchEventsKey(key);
-    // setCurrentPage(1);
-  };
-  // const [colored,setColored] = useState(true)
-  // const [coloredscd,setColoredScd] = useState(false)
-
-  // function allBtn(){
-  //   setColored(true)
-  //   setColoredScd(false)
-  // }
-
-  // function comedyBtn(){
-  //   setColored(false)
-  //   setColoredScd(true)
-  // }
 
   const categoryEvents =
     category === "all"
@@ -47,21 +28,16 @@ export default function EventsPage() {
         );
 
   console.log(categoryEvents);
-  const filteredSearchEvents = !searchEventsKey
-    ? categoryEvents
-    : categoryEvents.filter((event) =>
-        event.title.toLowerCase().includes(searchEventsKey.toLowerCase())
-      );
   const pageSize = 6;
   const pages = generateArrayFromNumber(
-    Math.ceil(filteredSearchEvents.length / pageSize)
+    Math.ceil(filteredSearch.length / pageSize)
   );
   console.log(pages);
 
   const pageToStart = (currentPage - 1) * pageSize;
   console.log(pageToStart);
 
-  const paginatedEvents = filteredSearchEvents.slice(
+  const paginatedEvents = filteredSearch.slice(
     pageToStart,
     pageToStart + pageSize
   );
@@ -104,47 +80,6 @@ export default function EventsPage() {
             <button className="absolute right-4 bottom-2 ">
               <img src="/images/Search.svg" alt="searchIcon" />
             </button>
-          </div>
-          {/* Navbar Search */}
-          <div className="w-full form-control hidden relative mb-4">
-            <input
-              onChange={(e) => handleSearchEventsKeyChanges(e.target.value)}
-              type="text"
-              placeholder="Search"
-              className="input focus:outline-main-color focus:outline-offset-0 text-white text-sm pb-1 input-bordered h-8 rounded-lg lg:w-72 md:w-48  bg-[rgba(201,201,201,0.2)] focus:border-none focus:outline-none"
-            />
-            <button className="absolute right-4 bottom-2">
-              <img src="/images/Search.svg" alt="searchIcon" />
-            </button>
-          </div>
-          {/* search dropdown */}
-          <div className="flex justify-center">
-            {searchEventsKey && (
-              <div className="block absolute text-white font-Inter bg-input md:w-96 z-50 my-1 rounded-lg px-2 py-2">
-                {filteredSearchEvents.length > 0 ? (
-                  <>
-                    {paginatedEvents.map((event, index) => {
-                      return (
-                        <div
-                          key={index}
-                          className="py-2 px-2 cursor-pointer"
-                          onClick={() => navigate(`/event-details/${event.id}`)}
-                        >
-                          <div className="flex items-center gap-2">
-                            <img className="w-10" src={event.imgUrl} alt="" />
-                            {event.title}
-                          </div>
-                        </div>
-                      );
-                    })}
-                  </>
-                ) : (
-                  <>
-                    <div className="flex justify-center">No events found</div>
-                  </>
-                )}
-              </div>
-            )}
           </div>
           <div className="w-full flex gap-5 items-center mb-7 overflow-x-auto">
             <div>
