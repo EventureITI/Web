e143cb2c7310adcef6827004a50c79beb7d7bb7c
Abdallah <abdallahmamdouh.eng@gmail.com>
2024-09-08 23:36:33 +0300

    style: update main color and navbar skeleton

--- src/Components/CategoryBtn.jsx
+++ src/Components/CategoryBtn.jsx
@@ -1,12 +1,12 @@
 import { NavLink } from "react-router-dom";
 
-export default function CategoryBtn({ category, path, classes }) {
+export default function CategoryBtn({ category, path }) {
   return (
     <NavLink
       to={path}
       style={{ height: "30px", minWidth: "80px" }}
       className={({ isActive }) =>
-        `text-uppercase flex justify-center items-center ${classes} hover:bg-main-hover text-white focus:outline-none font-medium rounded-full text-sm px-8 py-2.5 text-center mb-2 ${
+        `text-uppercase flex justify-center items-center hover:bg-main-color text-white focus:outline-none font-medium rounded-full text-sm px-8 py-2.5 text-center mb-2 ${
           isActive ? " bg-main-color" : "bg-zinc-700"
         }`
       }
