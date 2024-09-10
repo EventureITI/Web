b3853fd3dbda311b7591ef6bc6223bbf964600ff
Abdallah <abdallahmamdouh.eng@gmail.com>
2024-09-10 03:40:02 +0300

    refactor: refactor unusable imports in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import Home from "./Pages/Home";
 import EventsPage from "./Pages/EventsPage";
@@ -12,16 +12,15 @@ import ScrollTop from "./Components/ScrollTop";
 import EventDetails from "./Pages/EventDetails";
 import Footer from "./Components/Footer";
 import NotFound from "./Pages/NotFound";
-import BackTop from "./Components/BackTop";
 import PaymentSuccess from "./Pages/payment/PaymentSuccess";
 import PaymentFailed from "./Pages/payment/PaymentFailed";
 import GetTicket from "./Pages/GetTicket";
 import Profile from "./Pages/Profile";
 import SuccessEmail from "./Pages/SuccessEmail";
 import AuthDetailsContext from "./context/Authentication/AuthDetailsContext";
-import { useContext } from "react";
 import NotAuthenticated from "./Components/ProtectedRoutes/NotAuthenticated";
 import Authenticated from "./Components/ProtectedRoutes/Authenticated";
+
 
 function App() {
   const location = useLocation();
@@ -34,6 +33,7 @@ function App() {
     "/signup",
     "/",
   ]; // here you will add the desired route that you do not want to render footer with
+
   return (
     <>
       <AuthDetailsContext>
