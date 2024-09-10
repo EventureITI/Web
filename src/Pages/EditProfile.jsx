8ede00eff17f71308dd0d67b42cd03a501c8e856
Abdallah <abdallahmamdouh.eng@gmail.com>
2024-09-10 03:43:26 +0300

    feat: add edit user profile features with validation

--- src/Pages/EditProfile.jsx
+++ src/Pages/EditProfile.jsx
@@ -1,34 +1,129 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import EyeIcon from "../Components/Icons/EyeIcon";
 import EyeSlashIcon from "../Components/Icons/EyeSlashIcon";
 import { useNavigate } from "react-router-dom";
+import { Validation } from "../context/Authentication/ValidationContext";
+import { auth, db, storage } from "../firebase/firebase-config";
+import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+import { updateEmail, updatePassword } from "firebase/auth";
+import { v4 as uuid } from "uuid";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import NameValidation from "../Components/validations/NameValidation";
+import EmailValidation from "../Components/validations/EmailValidation";
+import PassValidation from "../Components/validations/PassValidation";
+import ConfirmPassValidation from "../Components/validations/ConfirmPassValidation";
+import SignupPassValidation from "../Components/validations/SignupPassValidation";
 
 export default function EditProfile() {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [editPic, setEditPic] = useState(false);
-  const [uploadURL,setUploadURL] = useState("/images/profilePic.jpg")
-  const navigate = useNavigate()
+  const [userInfoImg, setUserInfoImg] = useState("");
+  const [userInfoId, setUserInfoId] = useState("");
 
-  function uploaded(e){
-    const reader = new FileReader();
-      reader.onloadend = () => {
-        setUploadURL(reader.result); //made a path for your image
-      };
-      reader.readAsDataURL(e.target.files[0]); //read image as url
+  const {
+    userEmail,
+    setUserEmail,
+    userPass,
+    nameState,
+    setNameState,
+    emailState,
+    setEmailState,
+    passState,
+    confirmPassState,
+    userFirstName,
+    setUserFirstName,
+    userLastName,
+    setUserLastName,
+    minNameState,
+    noNumNameState,
+    requiredNameState,
+    correctEmailState,
+    editEmailRequireState,
+    correctPassState,
+    minPassState,
+    correctConfirmPassState,
+    handleFirstName,
+    handleLastName,
+    handleEmail,
+    handlePass,
+    handleConfirmPass,
+  } = useContext(Validation);
+  const navigate = useNavigate();
+
+  // save profile edits
+  async function handleEditProfile(e) {
+    e.preventDefault();
+    try {
+      if (
+        !requiredNameState &&
+        !noNumNameState &&
+        !minNameState &&
+        !editEmailRequireState &&
+        !correctEmailState
+      ) {
+        await updateDoc(doc(db, "user", userInfoId), {
+          firstName: userFirstName,
+          lastName: userLastName,
+          email: userEmail,
+          imgURL: userInfoImg,
+        });
+        // await updateEmail(auth.currentUser, userEmail);
+        await updatePassword(auth.currentUser, userPass);
+        toast.success("Your edits are saved successfully!", {
+          icon: <img src="/images/carbon_user-avatar-filled.svg"></img>,
+          progressStyle: { background: "white" },
+          style: { backgroundColor: "#00796B", color: "white" },
+        });
+        navigate("/");
+      } else {
+        setNameState(true);
+        setEmailState(true);
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error("Something went wrong, try again later");
+    }
   }
-  // useEffect(()=>{
-  //   console.log(uploadURL);
-    
-  // },[uploadURL])
-  
 
-  function picHover(){
-    setEditPic(true)
+  //Change img URL
+  const handleUploadImage = (e) => {
+    const imagesRef = ref(storage, `userImg/${uuid()}`);
+    uploadBytes(imagesRef, e.target.files[0]).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        // console.log(url);
+        setUserInfoImg(url);
+      });
+    });
+  };
+
+  useEffect(() => {
+    const setData = async () => {
+      const data = await getDocs(collection(db, "user"));
+      const userData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      const userInfo = userData.filter(
+        (e) => e.email == auth.currentUser.email
+      );
+      setUserInfoId(userInfo[0].id);
+      setUserFirstName(userInfo[0].firstName);
+      setUserLastName(userInfo[0].lastName);
+      setUserEmail(userInfo[0].email);
+      {
+        userInfo[0].imgURL != " "
+          ? setUserInfoImg(userInfo[0].imgURL)
+          : setUserInfoImg("/images/carbon_user-avatar-filled.svg");
+      }
+    };
+    setData();
+  }, []);
+
+  function picHover() {
+    setEditPic(true);
   }
 
-  function noPicHover(){
-    setEditPic(false)
+  function noPicHover() {
+    setEditPic(false);
   }
 
   return (
@@ -42,134 +137,217 @@ export default function EditProfile() {
             <p className="text-base font-medium block text-white">
               Profile Picture
             </p>
-            {/* <button className="text-main-color hover:text-main-hover overflow-hidden relative">
-              <p className="cursor-pointer">Edit</p>
-              <input
-                onChange={uploaded}
-                type="file"
-                className="absolute inset-0 w-full h-full opacity-0 z-10"
-              />
-            </button> */}
           </div>
           <div className="flex justify-center items-center w-full py-7">
-            <div onMouseEnter={picHover} onMouseLeave={noPicHover} className="rounded-full w-32 h-32 relative overflow-hidden"
-            style={{backgroundImage:`url(${uploadURL})`, backgroundSize:"cover"}}
+            <div
+              onMouseEnter={picHover}
+              onMouseLeave={noPicHover}
+              className="rounded-full w-32 h-32 relative overflow-hidden"
+              style={{
+                backgroundImage: `url(${userInfoImg})`,
+                backgroundSize: "cover",
+              }}
             >
               <input
-                onChange={uploaded}
+                onChange={handleUploadImage}
                 type="file"
                 className="absolute inset-0 w-full h-full opacity-0 z-10 cursor-pointer"
               />
-              {editPic && <div className="bg-bg-main opacity-45 w-full cursor-pointer rounded-full h-full absolute top-0 flex justify-center items-center">
-                <img src="/images/edit-text.png" alt="editIcon" />
-              </div>}
-
+              {editPic && (
+                <div className="bg-bg-main opacity-45 w-full cursor-pointer rounded-full h-full absolute top-0 flex justify-center items-center">
+                  <img src="/images/edit-text.png" alt="editIcon" />
+                </div>
+              )}
             </div>
           </div>
         </div>
-        <form>
+        <form onSubmit={handleEditProfile}>
           {/* First Name and Last Name */}
-          <div className="mb-8 grid grid-cols-2 gap-4">
-            <div>
-              <label
-                className="text-base font-medium block text-white mb-2"
-                htmlFor="firstName"
-              >
-                First name
-              </label>
-              <input
-                className="w-full px-4 py-3 bg-input text-white rounded-xl focus:outline-none focus:ring-2 focus:ring-teal-500"
-                type="text"
-                id="firstName"
-                placeholder="First name"
-                style={{ caretColor: "#4FE0D2" }}
-              />
+          <div className={`flex flex-col ${nameState ? "mb-0" : "mb-6"} `}>
+            <div className=" grid grid-cols-2 gap-4">
+              <div>
+                <label
+                  className="text-base font-medium block text-white mb-2"
+                  htmlFor="firstName"
+                >
+                  First name
+                </label>
+                <input
+                  className={`w-full px-4 py-3 bg-input text-white rounded-xl focus:outline-none focus:ring-2  ${
+                    !requiredNameState && !minNameState && !noNumNameState
+                      ? "focus:border-main-color focus:ring-main-color border-0"
+                      : `focus:border-red-800 focus:ring-red-800 ${
+                          nameState ? "border-[1px]" : "border-0"
+                        } border-red-800`
+                  }`}
+                  type="text"
+                  id="firstName"
+                  value={userFirstName}
+                  onChange={handleFirstName}
+                  placeholder="First name"
+                  style={{ caretColor: "#4FE0D2" }}
+                />
+              </div>
+              <div>
+                <label
+                  className="block text-white mb-2 text-base font-medium"
+                  htmlFor="lastName"
+                >
+                  Last name
+                </label>
+                <input
+                  className={`w-full px-4 py-3 bg-input text-white rounded-xl focus:outline-none focus:ring-2 ${
+                    !requiredNameState && !minNameState && !noNumNameState
+                      ? "focus:border-main-color focus:ring-main-color border-0"
+                      : `focus:border-red-800 focus:ring-red-800 ${
+                          nameState ? "border-[1px]" : "border-0"
+                        } border-red-800`
+                  }`}
+                  type="text"
+                  id="lastName"
+                  value={userLastName}
+                  onChange={handleLastName}
+                  placeholder="Last name"
+                  style={{ caretColor: "#4FE0D2" }}
+                />
+              </div>
             </div>
+            <NameValidation
+              nameState={nameState}
+              requiredNameState={requiredNameState}
+              minNameState={minNameState}
+              noNumNameState={noNumNameState}
+            />
+          </div>
+          {/* Email */}
+          <div
+            className={`flex font-Inter font-400 flex-col ${
+              emailState ? "mb-0" : "mb-6"
+            } `}
+          >
             <div>
               <label
                 className="block text-white mb-2 text-base font-medium"
-                htmlFor="lastName"
+                htmlFor="eventTitle"
               >
-                Last name
+                Email
               </label>
               <input
-                className="w-full px-4 py-3 bg-input text-white rounded-xl focus:outline-none focus:ring-2 focus:ring-teal-500"
-                type="text"
-                id="lastName"
-                placeholder="Last name"
+                className={`w-full px-4 py-3 bg-input  text-white rounded-xl focus:outline-none focus:ring-2 ${
+                  !editEmailRequireState && !correctEmailState
+                    ? "focus:border-main-color focus:ring-main-color border-0"
+                    : `focus:border-red-800 focus:ring-red-800 ${
+                        emailState ? "border-[1px]" : "border-0"
+                      } border-red-800`
+                }`}
+                type="email"
+                id="email"
+                value={userEmail}
+                onChange={handleEmail}
+                placeholder="example@email.com"
                 style={{ caretColor: "#4FE0D2" }}
               />
             </div>
-          </div>
-          {/* Email */}
-          <div className="mb-8">
-            <label
-              className="block text-white mb-2 text-base font-medium"
-              htmlFor="eventTitle"
-            >
-              Email
-            </label>
-            <input
-              className="w-full px-4 py-3 bg-input  text-white rounded-xl focus:outline-none focus:ring-2 focus:ring-teal-500"
-              type="email"
-              id="email"
-              placeholder="ahmed@gmail.com"
-              style={{ caretColor: "#4FE0D2" }}
+            <EmailValidation
+              emailState={emailState}
+              editEmailRequireState={editEmailRequireState}
+              correctEmailState={correctEmailState}
             />
           </div>
           {/* Password */}
-          <div className="mb-8 relative">
-            <label
-              className="block text-white mb-2 text-base font-medium"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <div className="relative">
-              <input
-                className="w-full px-4 py-3 bg-input text-white rounded-xl focus:outline-none focus:ring-2 focus:ring-teal-500 pr-10"
-                type={showPassword ? "text" : "password"}
-                id="password"
-                placeholder="**********"
-                style={{ caretColor: "#4FE0D2" }}
-              />
-              <span
-                onClick={() => setShowPassword(!showPassword)}
-                className="cursor-pointer absolute right-0 bottom-3 mr-3 flex items-center mt-8"
+          <div
+            className={`flex font-Inter font-400 flex-col ${
+              passState ? "mb-0" : "mb-6"
+            } `}
+          >
+            <div className=" relative">
+              <label
+                className="block text-white mb-2 text-base font-medium"
+                htmlFor="password"
               >
-                {showPassword ? <EyeIcon /> : <EyeSlashIcon />}
-              </span>
+                New Password
+              </label>
+              <div className="relative">
+                <input
+                  className={`w-full px-4 py-3 bg-input text-white rounded-xl focus:outline-none focus:ring-2 pr-10 ${
+                    !minPassState && !correctPassState
+                      ? "focus:border-main-color focus:ring-main-color border-0"
+                      : `focus:border-red-800 ${
+                          passState ? "border-[1px]" : "border-0"
+                        } focus:ring-red-800 border-red-800`
+                  }`}
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  placeholder="**********"
+                  onChange={handlePass}
+                  style={{ caretColor: "#4FE0D2" }}
+                />
+                <span
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="cursor-pointer absolute right-0 bottom-3 mr-3 flex items-center mt-8"
+                >
+                  {showPassword ? <EyeIcon /> : <EyeSlashIcon />}
+                </span>
+              </div>
             </div>
+            <PassValidation
+              passState={passState}
+              minPassState={minPassState}
+              correctPassState={correctPassState}
+            />
           </div>
           {/*Confirm Password */}
-          <div className="mb-10 relative">
-            <label
-              className="block text-white mb-2 text-base font-medium"
-              htmlFor="confirmPassword"
-            >
-              Confirm Password
-            </label>
+          <div
+            className={`flex font-Inter font-400 flex-col ${
+              confirmPassState ? "mb-0" : "mb-6"
+            } `}
+          >
             <div className="relative">
-              <input
-                className="w-full px-4 py-3 bg-input text-white rounded-xl focus:outline-none focus:ring-2 focus:ring-teal-500 pr-10"
-                type={showConfirmPassword ? "text" : "password"}
-                id="confirmPassword"
-                placeholder="**********"
-                style={{ caretColor: "#4FE0D2" }}
-              />
-              <span
-                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                className="cursor-pointer absolute right-0 bottom-3 mr-3 flex items-center mt-8"
+              <label
+                className="block text-white mb-2 text-base font-medium"
+                htmlFor="confirmPassword"
               >
-                {showConfirmPassword ? <EyeIcon /> : <EyeSlashIcon />}
-              </span>
+                Confirm New Password
+              </label>
+              <div className="relative">
+                <input
+                  className={`w-full px-4 py-3 bg-input text-white rounded-xl focus:outline-none focus:ring-2 pr-10 ${
+                    !correctConfirmPassState
+                      ? "focus:border-main-color focus:ring-main-color border-0"
+                      : `focus:border-red-800 focus:ring-red-800 ${
+                          confirmPassState ? "border-[1px]" : "border-0"
+                        } border-red-800`
+                  }`}
+                  type={showConfirmPassword ? "text" : "password"}
+                  id="confirmPassword"
+                  onChange={handleConfirmPass}
+                  placeholder="**********"
+                  style={{ caretColor: "#4FE0D2" }}
+                />
+                <span
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  className="cursor-pointer absolute right-0 bottom-3 mr-3 flex items-center mt-8"
+                >
+                  {showConfirmPassword ? <EyeIcon /> : <EyeSlashIcon />}
+                </span>
+              </div>
             </div>
+            <ConfirmPassValidation
+              confirmPassState={confirmPassState}
+              correctConfirmPassState={correctConfirmPassState}
+            />
           </div>
           <div className="flex justify-end space-x-6">
-            <button onClick={()=>navigate("/")} className=" w-[348px] text-white font-bold py-2 transition duration-300 ease-in-out rounded-2xl border-2 border-teal-500 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-teal-500">
+            <button
+              onClick={() => navigate("/")}
+              className=" w-[348px] text-white font-bold py-2 transition duration-300 ease-in-out rounded-2xl border-2 border-teal-500 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-teal-500"
+            >
               Cancel
             </button>
-            <button className=" w-[348px] bg-main-color hover:bg-main-hover transition duration-300 ease-in-out text-white font-bold py-2 px-6 rounded-2xl">
+            <button
+              type="submit"
+              className=" w-[348px] bg-main-color hover:bg-main-hover transition duration-300 ease-in-out text-white font-bold py-2 px-6 rounded-2xl"
+            >
               Continue
             </button>
           </div>
