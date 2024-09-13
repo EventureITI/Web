be43aba4f90ef056f38970868157f1c3f4ed4573
Amr Assem <amr.assem@onmail.com>
2024-09-13 20:49:00 +0300

    style: update forgot password card style

--- src/Components/ForgotPass.jsx
+++ src/Components/ForgotPass.jsx
@@ -1,12 +1,15 @@
 import React, { useContext, useState } from "react";
 import { Validation } from "../context/Authentication/ValidationContext";
 import SignupEmailValidation from "./validations/SignupEmailValidation";
-import { fetchSignInMethodsForEmail, sendPasswordResetEmail } from "firebase/auth";
+import {
+  fetchSignInMethodsForEmail,
+  sendPasswordResetEmail,
+} from "firebase/auth";
 import { auth } from "../firebase";
 import { toast } from "react-toastify";
 
 export default function ForgotPass() {
-    const [successState,setSuccessState] = useState(false)
+  const [successState, setSuccessState] = useState(false);
   const {
     userEmail,
     handleEmail,
@@ -19,14 +22,13 @@ export default function ForgotPass() {
   async function handleForgotPass(e) {
     e.preventDefault();
     try {
-        if(!requiredEmailState&&!correctEmailState){
-                await sendPasswordResetEmail(auth, userEmail)
-                setSuccessState(true)
-        }else{
-            setEmailState(true)
-        }
+      if (!requiredEmailState && !correctEmailState) {
+        await sendPasswordResetEmail(auth, userEmail);
+        setSuccessState(true);
+      } else {
+        setEmailState(true);
+      }
     } catch (err) {
-
       toast.error("Something went wrong, try again later", {
         icon: <img src="/images/carbon_user-avatar-filled.svg"></img>,
         progressStyle: { background: "white" },
@@ -39,14 +41,19 @@ export default function ForgotPass() {
     }
   }
   return (
-    <div className="w-full px-4 sm:px-20 md:px-32 xl:px-60 h-screen bg-sec-color flex justify-center items-center">
-      <div className="w-full border rounded-lg border-main-color p-4 bg-zinc-800 bg-opacity-50">
-        <h2 className="text-main-color font-Inter font-400 text-2xl text-center mb-5">
+    <div className="min-h-screen bg-bg-main px-4 sm:px-20 md:px-40 lg:px-64 xl:px-96 flex justify-center items-center">
+      <div className="w-full h-fit px-10 py-10 bg-ticket rounded-2xl flex-col justify-center items-start ">
+        <h2 className="text-white font-Inter font-400 text-2xl text-center mb-5">
           Reset Password
         </h2>
-        {successState && <div className="w-full py-10 p-5 sm:py-0 text-center flex flex-col justify-center items-center h-12 rounded-lg text-white font-Inter mb-5 bg-green-500 bg-opacity-60">
-            <p>A password reset link has been sent to your email if it's registered!</p>
-        </div>}
+        {successState && (
+          <div className="w-full py-10 p-5 sm:py-0 text-center flex flex-col justify-center items-center h-12 rounded-lg text-white font-Inter mb-5 bg-green-500 bg-opacity-60">
+            <p>
+              A password reset link has been sent to your email if it's
+              registered!
+            </p>
+          </div>
+        )}
         <form onSubmit={handleForgotPass}>
           <div
             className={`flex font-Inter font-400 flex-col ${
@@ -74,7 +81,7 @@ export default function ForgotPass() {
                       } border-red-800`
                 }
                     
-                    mt-1 rounded-lg bg-input p-3 w-full text-white text-base font-400 shadow-custom-shadow focus:outline-none focus:ring-1 focus:ring-main-color focus:border-main-color`}
+                    mt-1 rounded-lg bg-sec-color p-3 w-full text-white text-base font-400 shadow-custom-shadow focus:outline-none focus:ring-1 focus:ring-main-color focus:border-main-color`}
               />
             </div>
             <SignupEmailValidation
@@ -85,9 +92,9 @@ export default function ForgotPass() {
           </div>
           <button
             type="submit"
-            className="w-full h-12 font-medium text-white transition duration-300 ease-in-out border-[1px] border-main-color rounded-lg my-3 bg-sec-color bg-opacity-50 hover:bg-opacity-100"
+            className="bg-main-color hover:bg-main-hover transition duration-300 ease-in-out p-3 w-full rounded-lg text-white font-600"
           >
-            <p>Forgot Password</p>
+            <p>Reset Password</p>
           </button>
         </form>
       </div>
