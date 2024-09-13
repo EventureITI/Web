661705c9eed9708e7d3a43387a2dee3e729c07e7
Abdallah <abdallahmamdouh.eng@gmail.com>
2024-09-13 22:33:55 +0300

    fix: fixing skeleton color with adding skeleton for main banner

--- src/Components/Skeleton/MainSkeleton.jsx
+++ src/Components/Skeleton/MainSkeleton.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function MainSkeleton() {
   return (
-    <div className='relative flex justify-center w-full overflow-hidden' style={{maxHeight:"650px",minHeight:"350px"}} >
+    <div className='relative flex justify-center w-full overflow-hidden h-screen' style={{minHeight:"350px"}} >
       <div className='w-full h-full overflow-hidden ' >
-      <div className="flex items-center justify-center h-80 mb-4 bg-gray-300 rounded animate-pulse dark:bg-gray-700">
+      <div className="flex items-center justify-center h-screen mb-4 bg-[#1D232A] rounded animate-pulse dark:bg-gray-700">
         <svg className="w-10 h-20 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
             <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"/>
             <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
         </svg>
     </div>      </div>
       <div className='absolute bottom-0 w-full h-80 ' style={{background: "linear-gradient(360deg, rgba(26, 26, 26, 0.99) 30%, rgba(26, 26, 26, 0.90) 50%, rgba(26, 26, 26, 0.00) 100%)"}}></div>
-      <div className='absolute bottom-0 w-full px-6  sm:container sm:mx-auto sm:px-4'>
+      <div className='absolute bottom-0 w-full px-6 sm:container sm:mx-auto sm:px-4'>
         <div className='md-4 md:mb-3'>
         <div className="h-8 mb-5 md:h-8 bg-gray-200 rounded-lg animate-pulse dark:bg-gray-700 w-20"></div>
         </div>
