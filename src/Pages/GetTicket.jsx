f32a780059974d4be93bef0651db6356ae5ee0d7
Amr Assem <amr.assem@onmail.com>
2024-09-13 16:58:50 +0300

    feat: add purchased tickets to user document, and fix inconsistent UI

--- src/Pages/GetTicket.jsx
+++ src/Pages/GetTicket.jsx
@@ -1,90 +1,90 @@
-import React, { useContext, useState } from 'react'
-import AppContextProvider, { appContext } from '../context/AppContext';
-import { useParams } from 'react-router-dom';
-import formatDate from '../utils/formatDayAndYear';
-import { addDoc, arrayUnion, doc, getFirestore, setDoc, updateDoc } from 'firebase/firestore';
-import { auth, db} from '../firebase';
-import { AuthDetails } from '../context/Authentication/AuthDetailsContext';
+import React, { useContext, useState } from "react";
+import AppContextProvider, { appContext } from "../context/AppContext";
+import { useNavigate, useParams } from "react-router-dom";
+import formatDate from "../utils/formatDayAndYear";
 
 export default function GetTicket() {
-    
-    const [count, setCount] = useState(1);
-    const { events , user } = useContext(appContext)
+  const [count, setCount] = useState(1);
+  const { events } = useContext(appContext);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    const { id } = useParams();
-    
-    const event = events.find(event => event.id === id) || {};
-    
-    const { price, endTime, startTime, title, startDate } = event;
-    const day = formatDate(startDate)['day'];
-    const year = formatDate(startDate)['year']; 
-    
-    const handleClick = () => {
-        const eventData = {
-          id: id,
-          totalPrice: count * price,
-          numberOfTickets: count 
-        };
-      
-        addEventsToFirestore(eventData);
-      };
-      
-    const addEventsToFirestore = async (eventData) => {
-        
-        const id = user[0]['id'];
-        
-          const docRef = doc(db , `user/${id}`);
-    
-          try {
-            await updateDoc(docRef, { 
-                events: arrayUnion(eventData)
-              });
+  const event = events.find((event) => event.id === id) || {};
 
-          } catch (error) {
-            console.error('Error adding events: ', error);
-          }
-      };
-      
-    
-    return (
-        <div className='flex justify-center pt-28 pb-10 px-20'>
-            <div className="w-[600px] h-[600px] px-8 py-6 bg-ticket rounded-2xl flex-col justify-center items-center gap-4 flex">
-                <div className="self-stretch h-[175px] pb-4 border-b border-white/10 flex-col justify-start items-start gap-4 flex">
-                    <div className="self-stretch text-white text-lg font-normal font-['Inter']">Select Ticket</div>
-                    <div className="self-stretch h-[77px] pb-3 flex-col justify-start items-start gap-2 flex">
-                        <div className="self-stretch text-white text-base font-normal font-['Inter']">{title}</div>
-                        <div className="flex-col justify-start items-start gap-1 flex">
-                            <div className="text-main-color text-sm font-normal font-['Inter']">{startTime} - {endTime}</div>
-                            <div className="text-main-color text-sm font-normal font-['Inter']">{day}, {year}</div>
-                        </div>
-                    </div>
-                    <div className="self-stretch py-0.5 justify-between items-center inline-flex">
-                        <div className="text-white text-base font-normal font-['Inter']">{price} EGP</div>
-                        <div className="h-6 rounded justify-center items-center gap-3.5 flex">
-                            <button onClick={() => count > 1 ? setCount(count - 1) : null} className="w-6 h-6">
-                                <img src="/minus.svg" alt="minus" />
-                            </button>
-                            <div className="text-center text-white text-base font-normal font-['Inter']">{count}</div>
-                            <button onClick={() => setCount(count + 1)} className="w-6 h-6">
-                                <img src="/plus.svg" alt="plus" />
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div className="self-stretch grow shrink basis-0 flex-col justify-start items-start gap-4 flex">
-                    <div className="self-stretch text-white text-lg font-normal font-['Inter']">Payment Method</div>
-                    <div className="self-stretch justify-between items-center inline-flex">
-                        <div className="text-white text-base font-normal font-['Inter']">Stripe</div>
-                        <button>
-                            <img src="/stripe-s.svg" alt="plus" />
-                        </button>
-                    </div>
-                </div>
-                <button
-                onClick={handleClick} 
-                className="w-[348px] h-[51px] flex justify-center items-center bg-main-color hover:bg-main-hover transition duration-300 ease-in-out text-white font-bold py-4 px-6 rounded-2xl">Pay Now - {price * count} EGP</button>
+  const { price, endTime, startTime, title, startDate } = event;
+  const day = formatDate(startDate)["day"];
+  const year = formatDate(startDate)["year"];
 
+  const handleClick = () => {
+    const eventData = {
+      id: id,
+      title: title,
+      totalPrice: count * price,
+      numberOfTickets: count,
+    };
+    navigate(`/payment`, { state: eventData });
+  };
+
+  return (
+    <div className="flex justify-center pt-28 pb-10 px-20">
+      <div className="w-[600px] h-[600px] px-8 py-6 bg-ticket rounded-2xl flex-col justify-center items-center gap-4 flex">
+        <div className="self-stretch h-[175px] pb-4 border-b border-white/10 flex-col justify-start items-start gap-4 flex">
+          <div className="self-stretch text-white text-lg font-normal font-['Inter']">
+            Select Ticket
+          </div>
+          <div className="self-stretch h-[77px] pb-3 flex-col justify-start items-start gap-2 flex">
+            <div className="self-stretch text-white text-base font-normal font-['Inter']">
+              {title}
             </div>
+            <div className="flex-col justify-start items-start gap-1 flex">
+              <div className="text-main-color text-sm font-normal font-['Inter']">
+                {startTime} - {endTime}
+              </div>
+              <div className="text-main-color text-sm font-normal font-['Inter']">
+                {day}, {year}
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch py-0.5 justify-between items-center inline-flex">
+            <div className="text-white text-base font-normal font-['Inter']">
+              {price} EGP
+            </div>
+            <div className="h-6 rounded justify-center items-center gap-3.5 flex">
+              <button
+                onClick={() => (count > 1 ? setCount(count - 1) : null)}
+                className="w-6 h-6"
+              >
+                <img src="/minus.svg" alt="minus" />
+              </button>
+              <div className="text-center text-white text-base font-normal font-['Inter']">
+                {count}
+              </div>
+              <button onClick={() => setCount(count + 1)} className="w-6 h-6">
+                <img src="/plus.svg" alt="plus" />
+              </button>
+            </div>
+          </div>
         </div>
-    )
+        <div className="self-stretch grow shrink basis-0 flex-col justify-start items-start gap-4 flex">
+          <div className="self-stretch text-white text-lg font-normal font-['Inter']">
+            Payment Method
+          </div>
+          <div className="self-stretch justify-between items-center inline-flex">
+            <div className="text-white text-base font-normal font-['Inter']">
+              Stripe
+            </div>
+            <button>
+              <img src="/stripe-s.svg" alt="plus" />
+            </button>
+          </div>
+        </div>
+        <button
+          onClick={handleClick}
+          className="w-[348px] h-[51px] flex justify-center items-center bg-main-color hover:bg-main-hover transition duration-300 ease-in-out text-white font-bold py-4 px-6 rounded-2xl"
+        >
+          Proceed to Pay {price * count} EGP
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/Pages/payment/PaymentSuccess.jsx
+++ src/Pages/payment/PaymentSuccess.jsx
@@ -1,19 +1,58 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { appContext } from "../../context/AppContext";
+import { db } from "../../firebase";
+import { Link } from "react-router-dom";
 
 export default function PaymentSuccess() {
+  const [eventData, setEventData] = useState(null);
+  const { user } = useContext(appContext);
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const eventDataParam = queryParams.get("eventData");
+    if (eventDataParam) {
+      setEventData(JSON.parse(decodeURIComponent(eventDataParam)));
+    }
+  }, [location]);
+
+  const addEventsToFirestore = async (eventData) => {
+    const id = user[0]["id"];
+    const docRef = doc(db, `users/${id}`);
+    try {
+      await updateDoc(docRef, {
+        events: arrayUnion(eventData),
+      });
+    } catch (error) {
+      console.error("Error adding events: ", error);
+    }
+  };
+
+  if (eventData) {
+    addEventsToFirestore(eventData);
+  }
+
   return (
-    <div className="px-20 pb-16 pt-[114px] flex-col justify-center items-center gap-8 flex">
-    <div className="w-[307px] h-[225px] ">
-       <img src="/images/success.svg" alt="" srcset="" /> 
+    <div className="bg-bg-main min-h-screen px-20 pb-16 pt-[114px] flex-col justify-center items-center gap-8 flex">
+      <div className="w-[307px] h-[225px] ">
+        <img src="/images/success.svg" alt="" />
+      </div>
+      <div className="w-[356px] flex-col justify-start items-center gap-4 flex">
+        <div className="text-center text-white text-2xl font-bold font-['Inter']">
+          Payment Successful
+        </div>
+        <div className="self-stretch text-center text-main-color text-base font-normal font-['Inter']">
+          Your event ticket has been booked and is available in your profile
+        </div>
+      </div>
+      <div className="flex-col justify-center items-center gap-4 flex">
+        <Link to={"/profile"} className="w-[348px] h-[51px] flex justify-center items-center bg-main-color hover:bg-main-hover transition duration-300 ease-in-out text-white font-bold py-4 px-6 rounded-2xl">
+          View My Tickets
+        </Link>
+        <Link to={`/event/${eventData.id}`} className=" w-[348px]   h-[51px]  flex justify-center items-center text-white font-bold py-4 px-6 transition duration-300 ease-in-out rounded-2xl border-2 border-teal-500 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-teal-500">
+          Back To Event
+        </Link>
+      </div>
     </div>
-    <div className="w-[356px] flex-col justify-start items-center gap-4 flex">
-        <div className="text-center text-white text-2xl font-bold font-['Inter']">Payment Successful</div>
-        <div className="self-stretch text-center text-main-color text-base font-normal font-['Inter']">Your event ticket has been booked and is available in your profile</div>
-    </div>
-    <div className="flex-col justify-center items-center gap-4 flex">
-            <button className="w-[348px] h-[51px] flex justify-center items-center bg-main-color hover:bg-main-hover transition duration-300 ease-in-out text-white font-bold py-4 px-6 rounded-2xl">View My Tickets</button>
-            <button className=" w-[348px]   h-[51px]  flex justify-center items-center text-white font-bold py-4 px-6 transition duration-300 ease-in-out rounded-2xl border-2 border-teal-500 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-teal-500">Back To Event</button>
-    </div>
-</div>
-  )
+  );
 }
